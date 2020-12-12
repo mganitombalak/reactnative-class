e500f46a70a38c45799b3e6325900d7bd3b507a4
Mehmet Gani Tombalak <m.ganitombalak@azerion.com>
2020-12-12 17:30:49 +0300

    FileSystem APi

--- baro-vision/screens/CameraScreen.js
+++ baro-vision/screens/CameraScreen.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View, Button, Image, StyleSheet, Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
+import * as FileSystem from 'expo-file-system';
 
 const CameraScreen = props => {
-
+    const [selectedImage, setSelectedImage] = useState();
     const checkPermissions = async () => {
-        const permissionResult = await Permissions.askAsync(Permissions.CAMERA);
+        const permissionResult = await ImagePicker.requestCameraRollPermissionsAsync(); //Permissions.askAsync(Permissions.CAMERA);
         if (!permissionResult.granted) { // permissionResult.status!=='granted'
             Alert.alert('Insufficient Permission!', 'Please grant access to your Camera!',
                 [{ text: 'OK' }]);
@@ -16,15 +17,26 @@ const CameraScreen = props => {
     }
 
     const takePic = async () => {
-        const hasPermission= await checkPermissions();
+        const hasPermission = await checkPermissions();
         if (hasPermission) {
-            ImagePicker.launchCameraAsync();
+            const imageResult = await ImagePicker.launchCameraAsync({
+                allowsEditing: true,
+                aspect: [16, 9],
+                quality: 0.5
+            });
+            console.log(`Original file:${imageResult.uri}`);
+            const fileName = imageResult.uri.split('/').pop();
+            const picPath = FileSystem.documentDirectory + fileName;
+            console.log(`Moved file:${picPath}`);
+            await FileSystem.moveAsync({from:imageResult.uri,to:picPath});
+            setSelectedImage(picPath);            
         }
     };
     return (<View>
         <View style={styles.imageContainer}>
-            <Text>Camera Access</Text>
-            <Image style={styles.image}></Image>
+            {selectedImage ?
+                (<Image style={styles.image} source={{ uri: selectedImage }} />) : (<Text>No Image Selected</Text>)
+            }
         </View>
         <Button title="Take Pic" onPress={takePic}></Button>
     </View>)
