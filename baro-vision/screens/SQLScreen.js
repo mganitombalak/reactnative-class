3056159fd7cc3026e29a287f5cfc904eb40f6485
Mehmet Gani Tombalak <m.ganitombalak@azerion.com>
2020-12-12 18:42:46 +0300

    Location start

--- baro-vision/screens/SQLScreen.js
+++ baro-vision/screens/SQLScreen.js
@@ -2,17 +2,20 @@ import React, { useState } from 'react';
 import { Text, View, FlatList, Button } from 'react-native';
 import * as SQLite from 'expo-sqlite';
 import * as EntityManager from '../sql/Database';
+import faker from 'faker';
+
+
 const SQLScreen = () => {
     const [screenData, setScreenData] = useState([]);
     const saveHandler = async () => {
-        const result = await EntityManager.insertData('Mehmet', 'Tombalak', 43.46, 23.45);
+        const result = await EntityManager.insertData(faker.name.firstName(), faker.name.lastName(), 43.46, 23.45);
     }
     const loadHandler = async () => {
         const result = await EntityManager.fetchBaroData();
         setScreenData(result.rows._array);
     }
     return (<View style={{flex:1}}>
-        <FlatList data={screenData} renderItem={(data)=> <Text>{data.item.firstName} {data.item.lastName}</Text>}></FlatList>
+        <FlatList data={screenData} renderItem={(data)=> <Text key={data.index}>{data.item.firstName} {data.item.lastName}</Text>}></FlatList>
         <Button title="Save" onPress={saveHandler}></Button>
         <Button title="Load" onPress={loadHandler}></Button>
     </View>)
