d185abf17b0e0e9642281f15fbf26ed0d6d646f0
Mehmet Gani Tombalak <m.ganitombalak@azerion.com>
2020-12-12 14:30:07 +0300

    Fix

--- baro-vision/screens/NewsDetail.js
+++ baro-vision/screens/NewsDetail.js
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 import {useSelector} from 'react-redux';
 const NewsDetail =props =>{
 
-    const selectedVideo= useSelector(state=>state.news.selectedVideo);
+    const selectedVideo= useSelector(state=>state.news.selected);
 return <View><Text>{selectedVideo.id}-{selectedVideo.title}</Text></View>
 }
 
--- baro-vision/store/reducer/news.js
+++ baro-vision/store/reducer/news.js
@@ -2,15 +2,15 @@
 
 import { SELECT_NEWS, SET_NEWS } from '../action/news';
 const initialState = {
-    videos: []
+    videos: [],
+    selected: null
 }
 
 const NewsReducer = (state = initialState, action) => {
-    console.log(state);
     switch (action.type) {
         case SELECT_NEWS:
             let s1 = { ...state };
-            s1.selectedVideo = state.videos.find(v => v.id === action.payload);
+            s1.selected = state.videos.length > 0 ? state.videos.find(v => v.id === action.payload) : null;
             return s1;
         case SET_NEWS:
             let s2 = { ...state };
